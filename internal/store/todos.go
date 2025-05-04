3cc0a7e8ac95e2d3b71c9d2a4bdfba09975f9e2b
Germán Valencia <machinarius92@gmail.com>
2025-05-04 09:59:08 -0500

    Pre-allocating the slice for more performance

--- internal/store/todos.go
+++ internal/store/todos.go
@@ -15,18 +15,12 @@ func GetAll() ([]*models.TodoItem, error) {
 		return nil, fmt.Errorf("could not load todos: %w", todosResult.Error)
 	}
 
-	var todos []*models.TodoItem
-	if len(todoRecords) == 0 {
-		// The loop won't be executed if the slice is empty so we need
-		// to initialize it here.
-		todos = []*models.TodoItem{}
-	} else {
-		// Mapping is idiomatically done in a loop, rather than a map function!
-		// The same applies to other higher-order functions like `filter`.
-		for _, todoRecord := range todoRecords {
-			// append will create a new slice if the slice is nil.
-			todos = append(todos, unwrapRecord(&todoRecord))
-		}
+	// Pre-allocating the slice with the right size is more efficient.
+	todos := make([]*models.TodoItem, len(todoRecords))
+	// Mapping is idiomatically done in a loop, rather than a map function!
+	// The same applies to other higher-order functions like `filter`.
+	for i := range todoRecords {
+		todos[i] = unwrapRecord(&todoRecords[i])
 	}
 	return todos, nil
 }
